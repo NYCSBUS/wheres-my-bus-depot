7e3f8a4a34c260d47bdb1ae0bbbe68f23b863ea5
Varun Adibhatla <vr00n@users.noreply.github.com>
2024-08-21 20:13:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -101,7 +101,11 @@ def switch_to_nearest_tab():
         else:
             st.warning("You are not within any defined bus yard boundaries.")
 
-#switch_to_nearest_tab()
+# Enable or disable automatic tab switching
+enable_auto_switch = False  # Set to False to disable
+
+if enable_auto_switch:
+    switch_to_nearest_tab()
 
 # Function to clean and normalize the vehicle name
 def clean_vehicle_name(vehicle_name):
